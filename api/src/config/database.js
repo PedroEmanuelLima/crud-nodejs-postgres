2301de7ac17d78db348547de2719965539ce5610
Pedro Almeida <pa69731@gmail.com>
2021-01-14 14:54:16 -0300

    Estabelecida conxão de dados co o postgres.

---
+++ api/src/config/database.js
@@ -0,0 +1,24 @@
+/**
+* Arquivo: src/config/database.js
+* Descrição: arwuivo responsável pelas 'ConnectionString' da aplicação: PostgresSQL'.
+* Data: 14/01/2021
+* Autor: Pedro Emanuel
+*/
+
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ==> Conexão com a Base de Dados (PostgresSQL)
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+    console.log('Base de Dados conectada com sucesso!');
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text.params)
+}
